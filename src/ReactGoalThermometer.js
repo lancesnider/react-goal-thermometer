9223191542268870654ca273125d443ba280a5f7
Lance <lance@lancesnider.com>
2020-02-03 19:09:01 -0800

    Allow other number formats

--- src/ReactGoalThermometer.js
+++ src/ReactGoalThermometer.js
@@ -1,11 +1,55 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const ReactGoalThermometer = () => {
+const valueString = (value, props) => {
+  return Intl.NumberFormat(props.locale, {
+    style: 'currency',
+    currency: props.currency,
+    currencyDisplay: props.currencyDisplay,
+    minimumFractionDigits: props.roundNumbers ? 0 : 2,
+  }).format(value);
+}
+
+const ReactGoalThermometer = (props) => {
+  const { roundNumbers, amount, goal } = props;
+
+  const currentValue = roundNumbers ?
+    Math.round(amount) :
+    amount;
+
   return (
     <div>
-      Goal Thermometer
+      goal: {valueString(goal, props)}
+      <br />
+      current: {valueString(currentValue, props)}
     </div>
   );
 };
+
+ReactGoalThermometer.defaultProps = {
+  roundNumbers: true,
+  locale: 'en',
+  currency: 'USD',
+  currencyDisplay: 'symbol',
+  animationDuration: 1000,
+  numberOfTickmarks: 4,
+  textWithTickmarks: true,
+}
+
+ReactGoalThermometer.propTypes = {
+  goal: PropTypes.number.isRequired,
+  amount: PropTypes.number.isRequired,
+  roundNumbers: PropTypes.bool.isRequired,
+  numberOfTickmarks: PropTypes.number.isRequired,
+  textWithTickmarks: PropTypes.bool.isRequired,
+  locale: PropTypes.string.isRequired,
+  currency: PropTypes.string.isRequired,
+  currencyDisplay: PropTypes.oneOf([
+    'symbol',
+    'code',
+    'name',
+  ]),
+  animationDuration: PropTypes.number.isRequired,
+}
 
 export default ReactGoalThermometer;
--- src/ReactGoalThermometer.stories.js
+++ src/ReactGoalThermometer.stories.js
@@ -8,5 +8,41 @@ export default {
 };
 
 export const Default = () => (
-  <ReactGoalThermometer />
+  <ReactGoalThermometer
+    goal={10000}
+    amount={2513.441246}
+  />
+);
+
+export const NumberFormats = () => (
+  <div>
+    <ReactGoalThermometer
+      goal={10000}
+      amount={2513.441246}
+      roundNumbers={false}
+      locale='ja-JP'
+      currency='JPY'
+    />
+    <ReactGoalThermometer
+      goal={10000}
+      amount={2513.441246}
+      locale='de-DE'
+      currency='EUR'
+    />
+    <ReactGoalThermometer
+      goal={10000}
+      amount={2513.441246}
+      locale='de-DE'
+      currency='EUR'
+      currencyDisplay='code'
+    />
+    <ReactGoalThermometer
+      goal={10000}
+      amount={2513.441246}
+      roundNumbers={false}
+      locale='ja-JP'
+      currency='JPY'
+      currencyDisplay='name'
+    />
+  </div>
 );
